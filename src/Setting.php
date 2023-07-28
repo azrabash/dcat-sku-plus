d2324ebd7380f4fbd40792f575365630ec7872a8
azrabash <30510652+azrabash@users.noreply.github.com>
2023-07-28 15:17:39 +0700

    Update Setting.php

--- src/Setting.php
+++ src/Setting.php
@@ -8,19 +8,19 @@ class Setting extends Form
 {
     public function title()
     {
-        return 'SKU扩展增强版配置';
+        return 'SKU Extended Configuration';
     }
 
     public function form()
     {
-        $this->text('sku_plus_img_upload_url', '图片上传地址')
+        $this->text('sku_plus_img_upload_url', 'Image upload location')
             ->default('/admin/sku-image-upload')
-            ->help('必须以【/】开头')
+            ->help('Must start with【/】')
             ->required();
 
-        $this->text('sku_plus_img_remove_url', '图片删除地址')
+        $this->text('sku_plus_img_remove_url', 'Image deletion location')
             ->default('/admin/sku-image-remove')
-            ->help('必须以【/】开头')
+            ->help('Must start with【/】')
             ->required();
     }
 }
